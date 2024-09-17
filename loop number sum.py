f425e5f70a0946a9328651f03ce23022817df677
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-09-17 14:13:11 +0300

    Create loop number sum.py

---
+++ loop number sum.py
@@ -0,0 +1,12 @@
+"""QUESTION => Numbers can add up quickly! Write a Python program using a for loop to calculate the sum of all the numbers in a list.
+Create a list of numbers (e.g., numbers = [1, 5, 3, 9]).
+Initialize a variable total to 0, which will store the running sum.
+Use a for loop to iterate over the numbers list.
+Inside the loop, add the current number (use the loop variable) to the total variable.
+After the loop, print the final total value, which represents the sum of all the numbers in the list."""
+
+
+numbers = [0,1,2,3,4,5,6,7,8,9]
+for number in numbers :
+    print (f"{number + number}")
+
