7b20289248eceb597d01516c6616623732ebf83e
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-09-17 23:43:24 +0300

    Update Guess a secret number.py

--- Games/Guess a secret number.py
+++ Games/Guess a secret number.py
@@ -11,3 +11,42 @@ match secret_number :
             print ("OK GOOD BYE")
             
         
+
+
+import random
+
+secret_number = random.randint(1, 10)
+guess_count = 0
+
+while True:
+    guess = int(input("I'm thinking of a number between 1 and 10. Can you guess it? "))
+    guess_count += 1
+
+    if guess == secret_number:
+        print(f"Congratulations, you guessed it in {guess_count} tries!")
+        break
+    elif guess > secret_number:
+        print("Oops, your guess is a bit high. Try again!")
+    else:
+        print("Nope, your guess is a bit low. Give it another shot!")
+
+while True:
+    play_again = input("Play again? (yes/no): ").strip().lower()
+    
+    if play_again == "yes":
+        secret_number = random.randint(1, 10)
+        guess_count = 0
+        while True:
+            guess = int(input("I'm thinking of a number between 1 and 10. Can you guess it? "))
+            guess_count += 1
+
+            if guess == secret_number:
+                print(f"Congratulations, you guessed it in {guess_count} tries!")
+                break
+            elif guess > secret_number:
+                print("Oops, your guess is a bit high. Try again!")
+            else:
+                print("Nope, your guess is a bit low. Give it another shot!")
+    else:
+        print("Thanks for playing! Goodbye!")
+        break
