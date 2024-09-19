c90da30d58b7f88ee5bd6927c18b56aacc971823
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-09-19 16:04:46 +0300

    Update Square _Asterisks(*)by_while_loop.py

--- Patterns :)/Square _Asterisks(*)by_while_loop.py
+++ Patterns :)/Square _Asterisks(*)by_while_loop.py
@@ -1,8 +1,9 @@
-i = 1 
-j = 1
-while i < 3:
-    print ("sosi " ,end="")
-    i =i + 1
-    while j < 2:
-        print (" Ayele " ,end="")
-        j = j + 1
+num = int(input("Enter the number to see an astric square  "))
+i = 0
+while i < num:
+    j = 0
+    while j < num :
+        print ("*", end="")
+        j += 1
+    print() 
+    i+=1 
