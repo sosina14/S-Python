133a3f331840591ca27e9f3667bb3022c0d2775e
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-09-23 23:25:19 +0300

    Create 3.py

---
+++ Practice_Excerise/3.py
@@ -0,0 +1,9 @@
+x = int(input ("Enter a number : "))
+def checker(num):
+    if num % 2 == 0:
+        print (f"{num} is even nnumber ")
+    else:
+        print (f"{num} is odd number")
+
+checker(x)
+
