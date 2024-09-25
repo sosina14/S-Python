5c17a059a7c7b3ae65090d8093923abcc6db85fb
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-09-25 19:12:07 +0300

    Create calculater.py

---
+++ Practice_Excerise/Modules_Ex1/calculater.py
@@ -0,0 +1,17 @@
+#calculator.py
+def add(x, y):
+#Returns the sum of two numbers
+  return x + y
+
+def subtract(x,y):
+  return x - y
+
+def multiply(x,y):
+  return x * y
+
+def divide(x,y):
+  if x > y :
+    return x / y
+  
+  else:
+    print("ERROR")
